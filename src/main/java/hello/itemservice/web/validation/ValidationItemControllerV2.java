a28d362f8b1d4ab4c09ab81c314aa2a32cce2ac5
juntae ha <hjt1031@naver.com>
2022-04-14 20:54:02 +0900

    오류코드와 메시지 - ValiationUtill

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -127,11 +128,15 @@ public class ValidationItemControllerV2 {
     @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
+        // bindingResult.rejectValue("itemName", "required"); 랑 ValidationUtils 랑 같다. 한줄로 편하게 사용 가능. 단순한 기능만 제공
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
+
         //검증 로직
         if(!StringUtils.hasText(item.getItemName())){
             // rejectValue 는 필드에러를 대신 생성해준다.
             bindingResult.rejectValue("itemName", "required");
         }
+
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price","range", new Object[]{1000,1000000}, null);
         }
